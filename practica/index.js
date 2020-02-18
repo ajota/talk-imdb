f44fa04ee8b25f01bad172eed1cb4b49ce721afa
Anderson Jovanny Velez Alvarez <avelez@sistecredito.com>
2020-02-18 14:50:46 -0500

    Adjustment in example

--- practica/index.js
+++ practica/index.js
@@ -3,7 +3,7 @@ var loki = require('lokijs');
 //creating db conection
 var db = new loki('parctice.db');
 
-//creatin collection
+//collection creation 
 var users = db.addCollection('users');  
     users.insert([
 	    {name: 'user1', age: 50, dni: 11122},
