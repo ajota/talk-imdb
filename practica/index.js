09a848588042ec071349f5ed948e29068b0933fb
Ander velez <ajota06@gmail.com>
2020-02-18 00:18:09 -0500

    Added preactice

---
+++ practica/index.js
@@ -0,0 +1,16 @@
+var loki = require('lokijs');
+
+//creating db conection
+var db = new loki('parctice.db');
+
+//creatin collection
+var users = db.addCollection('users');  
+    users.insert([
+	    {name: 'user1', age: 50, dni: 11122},
+	    {name: 'user2', age: 50, dni: 22233},
+	    {name: 'user3', age: 50, dni: 33344}
+    ]);
+
+var results = users.find({age: 50});
+
+console.log(results);
